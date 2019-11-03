bf4e4880e207c8fc9b42b09d0451c00264e313f9
Balaji Shanmuga Sundaram <balaji.siva@gmail.com>
2019-11-03 22:17:27 +0530

    Challenge message

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,5 +1,7 @@
 const request = require('request')
 
+const message = "Chennai Rocks! ";
+
 const forecast = (latitude, longitude, callback) => {
     const url = 'https://api.darksky.net/forecast/efa3a101b60f76da440fcf5087adeccd/' + latitude + ',' + longitude
 
@@ -9,7 +11,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, message + body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
         }
     })
 }
